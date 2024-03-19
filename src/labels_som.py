d4aeab7721921d15aad95a150b6831270a071c49
Lucas Pires Bernardes <lucas.bernardes@toradex.com>
2024-03-19 12:36:16 -0300

    Add the labels for the som model trained
    Signed-off-by: Lucas Pires Bernardes <lucas.bernardes@toradex.com>

---
+++ src/labels_som.py
@@ -0,0 +1,28 @@
+#
+# Copyright 2020-2022 NXP
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+label2string = {
+		0:	"aeroplane",
+		1:	"bicycle",
+		2:	"bird",
+		3:	"boat",
+		4:	"bottle",
+		5:	"bus",
+		6:	"car",
+		7:	"cat",
+		8:	"chair",
+		9:	"cow",
+		10:	"diningtable",
+		11:	"dog",
+		12:	"horse",
+		13:	"motorbike",
+		14:	"person",
+		15:	"pottedplant",
+		16:	"sheep",
+		17:	"sofa",
+		18:	"som",
+		19:	"train",
+		20:	"tvmonitor"}
