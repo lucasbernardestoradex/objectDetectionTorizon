c2b652c3f36da0bf3e26f23ec337d40bf977ab83
Lucas Pires Bernardes <lucas.bernardes@toradex.com>
2024-04-10 13:31:58 -0300

    Fix libraries dependencies and improve python algorithm
    Signed-off-by: Lucas Pires Bernardes <lucas.bernardes@toradex.com>

--- src/main.py
+++ src/main.py
@@ -10,11 +10,56 @@ from PIL import Image
 import numpy as np
 import cv2
 import time, os
+from threading import Thread
 import argparse
 
 from labels_som import label2string
 
 MODEL_PATH = "/home/torizon/app/src/voc_som_eff2_30epoch.tflite"
+# MODEL_PATH = "voc_som_eff2_30epoch.tflite"
+
+# Define VideoStream class to handle streaming of video from webcam in separate processing thread
+# Source - Adrian Rosebrock, PyImageSearch: https://www.pyimagesearch.com/2015/12/28/increasing-raspberry-pi-fps-with-python-and-opencv/
+class VideoStream:
+    """Camera object that controls video streaming from the Picamera"""
+    def __init__(self,resolution=(640,480),framerate=30):
+        # Initialize the PiCamera and the camera image stream
+        self.stream = cv2.VideoCapture(args.input)
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(3,resolution[0])
+        ret = self.stream.set(4,resolution[1])
+            
+        # Read first frame from the stream
+        (self.grabbed, self.frame) = self.stream.read()
+
+	# Variable to control when the camera is stopped
+        self.stopped = False
+
+    def start(self):
+	# Start the thread that reads frames from the video stream
+        Thread(target=self.update,args=()).start()
+        return self
+
+    def update(self):
+        # Keep looping indefinitely until the thread is stopped
+        while True:
+            # If the camera is stopped, stop the thread
+            if self.stopped:
+                # Close camera resources
+                self.stream.release()
+                return
+
+            # Otherwise, grab the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+	# Return the most recent frame
+        return self.frame
+
+    def stop(self):
+	# Indicate that the camera and thread should be stopped
+        self.stopped = True
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -27,9 +72,16 @@ parser.add_argument(
     '--delegate',
     default='',
     help='delegate path')
+parser.add_argument(
+    '--threshold',
+    default=0.5,
+    help='Minimum confidence threshold for displaying detected objects')
+parser.add_argument(
+    '--resolution', 
+    default='1280x720',
+    help='Desired webcam resolution in WxH. If the webcam does not support the resolution entered, errors may occur.')
 args = parser.parse_args()
 
-vid = cv2.VideoCapture(args.input)
 
 if(args.delegate):
     ext_delegate = [tflite.load_delegate(args.delegate)]
@@ -44,73 +96,92 @@ output_details = interpreter.get_output_details()
 height = input_details[0]['shape'][1]
 width = input_details[0]['shape'][2]
 
-msg = ""
-total_fps = 0
-total_time = 0
+min_conf_threshold = float(args.threshold)
 
-ret, frame = vid.read()
-if (frame is None):
-    print("Can't read frame from source file ", args.input)
-    exit(0)
+# Check output layer name to determine if this model was created with TF2 or TF1,
+# because outputs are ordered differently for TF2 and TF1 models
+outname = output_details[0]['name']
 
-while ret:
-    total_fps += 1
-    loop_start = time.time()
+if ('StatefulPartitionedCall' in outname): # This is a TF2 model
+    boxes_idx, classes_idx, scores_idx = 1, 3, 0
+else: # This is a TF1 model
+    boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
-    img = cv2.resize(frame, (width,height)).astype(np.uint8)
-    input_data = np.expand_dims(img, axis=0)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
+# Initialize frame rate calculation
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
 
-    invoke_start = time.time()
+resW, resH = args.resolution.split('x')
+imW, imH = int(resW), int(resH)
+
+# Initialize video stream
+videostream = VideoStream(resolution=(imW,imH),framerate=30).start()
+time.sleep(1)
+
+#for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+while True:
+
+    # Start timer (for calculating frame rate)
+    t1 = cv2.getTickCount()
+
+    # Grab frame from video stream
+    frame1 = videostream.read()
+
+    # Acquire frame and resize to expected shape [1xHxWx3]
+    frame = frame1.copy()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_resized = cv2.resize(frame_rgb, (width, height))
+    input_data = np.expand_dims(frame_resized, axis=0)
+
+    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+    # if floating_model:
+    #     input_data = (np.float32(input_data) - input_mean) / input_std
+
+    # Perform the actual detection by running the model with the image as input
+    interpreter.set_tensor(input_details[0]['index'],input_data)
     interpreter.invoke()
-    invoke_end = time.time()
 
-    boxes = interpreter.get_tensor(output_details[1]['index'])[0]
-    labels = interpreter.get_tensor(output_details[3]['index'])[0]
-    scores = interpreter.get_tensor(output_details[0]['index'])[0]
-    number = interpreter.get_tensor(output_details[2]['index'])[0]
-    for i in range(int(number)):
-        if scores[i] > 0.5:
-            box = [boxes[i][0], boxes[i][1], boxes[i][2], boxes[i][3]]
-            x0 = max(2, int(box[1] * frame.shape[1]))
-            y0 = max(2, int(box[0] * frame.shape[0]))
-            x1 = int(box[3] * frame.shape[1])
-            y1 = int(box[2] * frame.shape[0])
+    # Retrieve detection results
+    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
 
-            cv2.rectangle(frame, (x0, y0), (x1, y1), (255, 0, 0), 2)
-            cv2.putText(frame, label2string[labels[i]], (x0, y0 + 13),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
-            print("rectangle:(%d,%d),(%d,%d) label:%s" % 
-                    (x0, y0, x1, y1, label2string[labels[i]]))
+    # Loop over all detections and draw detection box if confidence is above minimum threshold
+    for i in range(len(scores)):
+        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
 
-    loop_end = time.time()
-    total_time += (loop_end - loop_start)
+            # Get bounding box coordinates and draw box
+            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+            ymin = int(max(1,(boxes[i][0] * imH)))
+            xmin = int(max(1,(boxes[i][1] * imW)))
+            ymax = int(min(imH,(boxes[i][2] * imH)))
+            xmax = int(min(imW,(boxes[i][3] * imW)))
+            
+            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
 
-    invoke_time_seg = (invoke_end - invoke_start)
-    invoke_time_ms = invoke_time_seg * 1000
-    fps = 1/invoke_time_seg
+            # Draw label
+            object_name = label2string[int(classes[i])] # Look up object name from "labels" array using class index
+            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 
-    # Save the fps count in /tmp to be used by Device Monitoring in Cloud
-    file_path = "/tmp/fps.txt"
-    with open(file_path, 'w') as file:
-      file.write(str(fps))
+    # Draw framerate in corner of frame
+    cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
 
-    msg = "FPS:" + "{:.2f}".format(fps) + "  Invoke time:" + "{:.2f}".format(invoke_time_ms) + "ms"
-    cv2.putText(frame, msg, (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 3)
+    # All the results have been drawn on the frame, so it's time to display it.
+    cv2.imshow('Object detector', frame)
 
-    #Name the GUI app
-    cv2.namedWindow('object_detector',cv2.WINDOW_NORMAL)
-    
-    #Set the properties of GUI app
-    cv2.setWindowProperty('object_detector', cv2.WND_PROP_FULLSCREEN,
-                          cv2.WINDOW_FULLSCREEN)
+    # Calculate framerate
+    t2 = cv2.getTickCount()
+    time1 = (t2-t1)/freq
+    frame_rate_calc= 1/time1
 
-    cv2.imshow("object_detector", frame)
-
-    ret, frame = vid.read()
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Press 'q' to quit
+    if cv2.waitKey(1) == ord('q'):
         break
 
-time.sleep(2)
-vid.release()
+# Clean up
 cv2.destroyAllWindows()
+videostream.stop()
